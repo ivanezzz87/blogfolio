5a16a6f9d4e447d4db395ab39713f30354e86fc2
ivanezzz87 <mseptk@yandex.ru>
2025-09-18 07:27:35 +0300

    feat: add input component

--- blogfolio/src/App.tsx
+++ blogfolio/src/App.tsx
@@ -1,53 +1,58 @@
-import React, { useState } from 'react';
-import './App.css'
-import './components/Button'
-import Button from './components/Button'
-import UserInfo from './components/UserInfo';
-import Title from './components/Title';
-import BurgerMenu from './components/Burger';
+import React, { useState } from "react";
+import "./App.css";
+import "./components/Button";
+import Button from "./components/Button";
+import UserInfo from "./components/UserInfo";
+import Title from "./components/Title";
+import BurgerMenu from "./components/Burger";
+import Input from "./components/Input";
 function App() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-      const toggleMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
   return (
     <div className="root-container">
-      <div className='buttons-container'>
+      <div className="buttons-container">
         <Button
           content="Primary"
           type="primary"
           state="enabled"
-          onClick={() => console.log('Уже нажата')}
+          onClick={() => console.log("Уже нажата")}
         />
         <Button
           content="Secondary"
           type="secondary"
           state="enabled"
-          onClick={() => console.log('Уже нажата')}
+          onClick={() => console.log("Уже нажата")}
         />
         <Button
           content="Secondary2"
           type="secondary2"
           state="enabled"
-          onClick={() => console.log('Уже нажата')}
+          onClick={() => console.log("Уже нажата")}
         />
       </div>
       <div className="userinfo-container">
-        <UserInfo
-          firstName="Ivan"
-          lastName="Dudko"
-        />
+        <UserInfo firstName="Ivan" lastName="Dudko" />
       </div>
       <div className="title-container">
-        <Title
-          text = "SignIn"
-        />
+        <Title text="SignIn" />
       </div>
       <div className="burger-container">
-                <BurgerMenu $isOpen={isMenuOpen} onClick={toggleMenu} />
+        <BurgerMenu $isOpen={isMenuOpen} onClick={toggleMenu} />
       </div>
+      <Input
+        type="text"
+        value="Text"
+        label="Text"
+        id="input1"
+        onChange={() => console.log("text")}
+        // isError
+        // textError="не заполнено"
+      />
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ blogfolio/src/components/Input.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import styled from "styled-components";
+
+interface IInput {
+  type: "text" | "email" | "password";
+  placeholder?: string;
+  value: string;
+  disabled?: boolean;
+  label?: string;
+  id?: string;
+  isError?: boolean;
+  textError?: string
+  onChange: (value: string) => void;
+}
+const Input: React.FC<IInput> = ({
+  type,
+  placeholder,
+  value,
+  disabled,
+  label,
+  id,
+  isError = false,
+  textError,
+  onChange,
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+  return (
+    <InputContainer>
+      {label && (
+        <StyledLabel htmlFor={id} $disabled={disabled}>
+          {label}
+        </StyledLabel>
+      )}
+      <StyledInput
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        disabled={disabled}
+        onChange={handleChange}
+        $isError={isError}
+      />
+      {isError && textError && (
+        <Error>
+          {textError}
+        </Error>
+      )}
+    </InputContainer>
+  );
+};
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: fit-content;
+  background-color: lightgray;
+  padding: 10px;
+`;
+const StyledLabel = styled.label<{ $disabled?: boolean }>`
+  font-size: 14px;
+  font-weight: 500;
+  text-align: left;
+  color: ${(props) => (props.$disabled ? "#999" : "#333")};
+  cursor: ${(props) => (props.$disabled ? "not-allowed" : "pointer")};
+`;
+const Error = styled.span`
+  font-size: 12px;
+  color: red;
+  text-align: left;
+  margin-top: -2px;
+`;
+
+const StyledInput = styled.input<{ $isError: boolean }>`
+  width: 450px;
+  padding: 12px 16px;
+  border: 2px solid #fff;
+  border-radius: 1px;
+  font-size: 16px;
+  transition: border-color 0.3s ease;
+  outline: none;
+
+  &:focus {
+    border-color: #999;
+    border-color: ${(props) => (props.$isError ? "red" : "#999")};
+  }
+
+  &:disabled {
+    background-color: #f5f5f5;
+    cursor: not-allowed;
+  }
+
+  &::placeholder {
+    color: #999;
+  }
+`;
+export default Input;
